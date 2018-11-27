8d5af4bf462f150688cf76f46196d0a592508535
Cheenue Yang <31903854+YangCheenue@users.noreply.github.com>
2018-11-27 14:52:48 -0600

    Added words to enemy objects

---
+++ TypeStarGame/Assets/Scripts/WordGenerator.cs
@@ -0,0 +1,74 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class WordGenerator : MonoBehaviour
+{
+
+    string[] easyWords = { "Sun", "Rock", "Moon", "Cheese",
+                           "Star", "Mars", "Of", "The",
+                           "Laser", "Rocket", "Alien", "Robot",
+                           "Earth", "Pluto", "Saturn", "Venus",
+                           "Hubble", "Gravity", "Apollo", "Meteor",
+                           "Galaxy", "Drax", "Thanos", "Punch"};
+
+    string[] mediumWords = { "spaceship", "Supernova", "Inertia", "Mercury",
+                             "Jupiter", "Sputnik", "Nebula", "Uranus",
+                             "Predator", "Kepler", "Pokemon", "Kuiper",
+                             "Challenger", "Discovery", "Millenium", "Armstrong",
+                             "Quasar", "Light-year", "Guardians", "Martian",
+                             "Kal-El", "Destroyer", "gauntlet", "Falcun"};
+
+    string[] difficultWords = { "Entropy", "Constellation", "Astronomy", "Astrology",
+                                "Magnetron", "Xenomorph", "Kryptoniter", "",
+                                "Astronaut", "Galileo", "Enterprise", "Pathfinder",
+                                "Andromeda", "Astrophysics", "Celestial", "Heliocentric",
+                                "Interstellar", "Syzygy", "Transneptunian", "Rocket Raccoon",
+                                "Frustum"};
+
+    string displayedText;
+    Text textBox;
+
+    private void Start()
+    {
+        textBox = GetComponent<Text>();
+        displayedText = HardRandomWords();
+        textBox.text = displayedText;
+    }
+
+    private void Update()
+    {
+        displayedText = HardRandomWords();
+        textBox.text = displayedText;
+    }
+
+    public string EasyRandomWords()
+    {
+        int ranIndexEasy = Random.Range(0, easyWords.Length);
+
+        string easyRandom = easyWords[ranIndexEasy];
+
+        return easyRandom;
+    }
+
+    public string MediumRandomWords()
+    {
+        int ranIndexMedium = Random.Range(0, mediumWords.Length);
+
+        string mediumRandom = mediumWords[ranIndexMedium];
+
+        return mediumRandom;
+    }
+
+    public string HardRandomWords()
+    {
+        int ranIndexDifficult = Random.Range(0, difficultWords.Length);
+
+        string difficultRandom = difficultWords[ranIndexDifficult];
+
+        return difficultRandom;
+    }
+
+}
+
