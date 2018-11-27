b8af4a85dd1c049c7a8edf7ef51d93cba3550c7e
Jacob Schwitzer <37703789+JacobSchwitzer@users.noreply.github.com>
2018-11-27 14:54:08 -0600

    Start for settings/music volume

---
+++ TypeStarGame/Assets/Scripts/UI Scripts/audioSlider.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class audioSlider : MonoBehaviour {
+
+    public Slider slider;
+    private GameObject audioObject;
+    private AudioSource audio;
+    private float startVal;
+    private float currentVal;
+
+    // Use this for initialization
+    void Start () {
+        audioObject = GameObject.FindGameObjectWithTag("music");
+        audio = audioObject.GetComponent<AudioSource>();
+        slider.value = 100;
+        startVal = slider.value;
+        
+
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        currentVal = slider.value;
+        if(startVal != currentVal)
+        {
+            startVal = currentVal;
+            audio.volume = startVal / 100;
+        }
+		
+	}
+}
---
+++ TypeStarGame/Assets/Scripts/UI Scripts/moveToNewScene.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+
+public class moveToNewScene : MonoBehaviour
+{
+
+    public Button button;
+    public string nextRoom;
+
+    // Use this for initialization
+    void Start()
+    {
+        //sets up button to work with a button object in game
+        Button btn = button.GetComponent<Button>();
+        btn.onClick.AddListener(TaskOnClick);
+
+        //unpauses game if coming back from pause menu
+        Time.timeScale = 1.0f;
+
+    }
+
+    //what to do when clicked
+    void TaskOnClick()
+    {
+
+        SceneManager.LoadScene(nextRoom);
+
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        //doesn't do anything
+    }
+
+}
