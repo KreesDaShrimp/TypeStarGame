89d6b59855f90c90b6223a568337f80666fec806
Cheenue Yang <31903854+YangCheenue@users.noreply.github.com>
2018-12-04 15:59:52 -0600

    Allowed words to be typed and destroyed

--- TypeStarGame/Assets/Scripts/TypeWord.cs
+++ TypeStarGame/Assets/Scripts/TypeWord.cs
@@ -10,7 +10,13 @@ public class TypeWord : MonoBehaviour
     private string theWord;
     private int currentIndex;
 
+    private Color defaultColor = Color.white;
+    private Color typingColor = new Color(0.4f, 0.8f, 1.0f);
+    private Color wrongColor = Color.red;
+
     List<GameObject> ufoList = new List<GameObject>();
+
+    private char typedLetter;
 
     private void Start()
     {
@@ -34,21 +40,47 @@ public class TypeWord : MonoBehaviour
 
     private void Update()
     {
+        // detect letter typed
         foreach (char letter in Input.inputString)
         {
             Typing(letter);
             Debug.Log(letter);
+            typedLetter = letter;
         }
+        
+
         Debug.Log(ufoList.Count);
         if (ufoList.Count > 0)
         {
-            ufoList[0].GetComponentInChildren<Text>().color = new Color(0.4f, 0.8f, 1.0f);
+            // ufoList[0].GetComponentInChildren<Text>().color = typingColor;
+            for (int i = 0; i < ufoList.Count; i++)
+            {
+                Text currentText = ufoList[i].GetComponentInChildren<Text>();
+
+                if (currentText.text.Length < 1)
+                {
+                    ufoList[i].SetActive(false);
+                    ufoList.Remove(ufoList[i]);
+                }
+                else
+                {
+                    string currentString = currentText.text;
+
+                    if (currentString[0] == typedLetter)
+                    {
+                        currentText.color = typingColor;
+                        currentString = currentString.Remove(0, 1);
+                        Debug.Log(currentString);
+                        currentText.text = currentString;
+                    }
+                }
+            }
         }
     }
     
+    // adds UFO object to list
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        Debug.Log("hi");
         ufoList.Add(collision.gameObject);
     }
 }
