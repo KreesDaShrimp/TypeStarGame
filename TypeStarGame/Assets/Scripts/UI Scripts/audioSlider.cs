ec75f089f02f0d5f91bfab7b4f665dde2219a1b6
Jacob Schwitzer <37703789+JacobSchwitzer@users.noreply.github.com>
2018-12-06 14:36:43 -0600

    Updated Settings to be one scene
    +brought settings into main menu
    +adjusted audioSlider script to handle audio value updates instead of in unity onValueChanged manager (wasn't working for some reason)
    +moved music from preload to mainMenu scene
    -removed settings scene
    -removed preload scene and associated code

--- TypeStarGame/Assets/Scripts/UI Scripts/audioSlider.cs
+++ TypeStarGame/Assets/Scripts/UI Scripts/audioSlider.cs
@@ -3,11 +3,24 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.Audio;
+using UnityEngine.UI;
 
 public class audioSlider : MonoBehaviour {
 
     public AudioMixer masterMixer;
-    
+    public Slider audSlider;
+
+    private void Update()
+    {
+        if(audSlider.name == "Sldr_Music")
+        {
+            audSlider.onValueChanged.AddListener(delegate { SetMusicLevel(audSlider.value); });
+        }
+        if(audSlider.name == "Sldr_SFX")
+        {
+            audSlider.onValueChanged.AddListener(delegate { SetSFXLevel(audSlider.value); });
+        }
+    }
     public void SetSFXLevel(float sfxLevel)
     {
         masterMixer.SetFloat("SFX", sfxLevel);
