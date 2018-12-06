d8c09c91ec11cd9d5fd44c3212d5efa92641176b
Cheenue Yang <31903854+YangCheenue@users.noreply.github.com>
2018-12-06 14:26:07 -0600

    Prevent player typing from affecting multiple enemies

--- TypeStarGame/Assets/Scripts/TypeWord.cs
+++ TypeStarGame/Assets/Scripts/TypeWord.cs
@@ -5,11 +5,6 @@ using UnityEngine.UI;
 
 public class TypeWord : MonoBehaviour
 {
-    private bool activeWord;
-    private char activeLetter;
-    private string theWord;
-    private int currentIndex;
-
     private Color defaultColor = Color.white;
     private Color typingColor = new Color(0.4f, 0.8f, 1.0f);
     private Color wrongColor = Color.red;
@@ -17,25 +12,12 @@ public class TypeWord : MonoBehaviour
     List<GameObject> ufoList = new List<GameObject>();
 
     private char typedLetter;
+    private GameObject activeWord;
+    private bool wordActivated;
 
     private void Start()
     {
-        currentIndex = 0;
-        // text.text = wordGenerator.displayedText;
-    }
-    public void Typing(char letter)
-    {
-        /*
-        if(letter == wordGenerator.displayedText[0])
-        {
-            //text.color = Color.green;
-        }
-        else
-        {
-            //text.color = Color.red;
-        }
-        */
-        currentIndex++;
+        wordActivated = false;
     }
 
     private void Update()
@@ -43,16 +25,12 @@ public class TypeWord : MonoBehaviour
         // detect letter typed
         foreach (char letter in Input.inputString)
         {
-            Typing(letter);
             Debug.Log(letter);
             typedLetter = letter;
         }
         
-
-        Debug.Log(ufoList.Count);
         if (ufoList.Count > 0)
         {
-            // ufoList[0].GetComponentInChildren<Text>().color = typingColor;
             for (int i = 0; i < ufoList.Count; i++)
             {
                 Text currentText = ufoList[i].GetComponentInChildren<Text>();
@@ -61,19 +39,34 @@ public class TypeWord : MonoBehaviour
                 {
                     ufoList[i].SetActive(false);
                     ufoList.Remove(ufoList[i]);
+                    wordActivated = false;
                 }
                 else
                 {
                     string currentString = currentText.text;
 
-                    if (currentString[0] == typedLetter)
+                    if (currentString[0] == typedLetter && !wordActivated)
                     {
-                        currentText.color = typingColor;
-                        currentString = currentString.Remove(0, 1);
-                        Debug.Log(currentString);
-                        currentText.text = currentString;
+                        activeWord = ufoList[i];
+                        wordActivated = true;
                     }
                 }
+            }
+        }
+
+        if (wordActivated)
+        {
+            Text currentText = activeWord.GetComponentInChildren<Text>();
+            string currentString = currentText.text;
+
+            currentText.color = typingColor;
+
+            Debug.Log(currentString);
+            if (currentString[0] == typedLetter)
+            {
+                currentString = currentString.Remove(0, 1);
+                currentText.text = currentString;
+                typedLetter = ' ';
             }
         }
     }
