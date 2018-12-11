34334058a1c545957c7951389aa1dec137cb82c9
Bryce Smrecek <dealer2077@gmail.com>
2018-12-11 14:19:00 -0600

    Game over
    Added a game over screen that takes the player back to the main menu

--- TypeStarGame/Assets/Enemy/Lives.cs
+++ TypeStarGame/Assets/Enemy/Lives.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Lives : MonoBehaviour {
 
@@ -55,6 +56,8 @@ public class Lives : MonoBehaviour {
         {
 
             Destroy(li2);
+
+            SceneManager.LoadScene("Game Over");
 
         }
 
