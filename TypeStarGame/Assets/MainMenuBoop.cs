b01bfdd44b4d4ee8b59d58aa15756d0b61e9bd23
Chris Schrempp <schremppc0870@my.uwstout.edu>
2018-12-19 01:00:32 -0600

    Added Sound to Main Menu Buttons

---
+++ TypeStarGame/Assets/MainMenuBoop.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainMenuBoop : MonoBehaviour {
+
+    public AudioSource Typing;
+    public AudioSource BadSelect;
+
+    public void Boop ()
+    {
+        Typing.Play();
+    }
+
+    public void BadBoop ()
+    {
+        BadSelect.Play();
+    }
+}
