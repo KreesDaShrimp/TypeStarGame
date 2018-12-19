fa6a9c29a312a61cae94ed9d7bbd0e415bec1516
kuekie <kuethao4880@gmail.com>
2018-12-19 01:07:38 -0600

    More words

--- TypeStarGame/Assets/Scripts/WordGenerator.cs
+++ TypeStarGame/Assets/Scripts/WordGenerator.cs
@@ -6,38 +6,63 @@ using UnityEngine.UI;
 public class WordGenerator : MonoBehaviour
 {
     private int currentLevel;
-    string[] easyWords = { "Sun", "Rock", "Moon", "Cheese",
-                           "Star", "Mars", "Of", "The",
-                           "Laser", "Rocket", "Alien", "Robot",
+    string[] easyWords = { "sun", "rock", "moon", "cheese",
+                           "star", "Mars", "of", "the",
+                           "laser", "rocket", "alien", "robot",
                            "Earth", "Pluto", "Saturn", "Venus",
-                           "Hubble", "Gravity", "Apollo", "Meteor",
-                           "Galaxy", "Drax", "Thanos", "Punch"};
+                           "hubble", "gravity", "Apollo", "meteor",
+                           "galaxy", "drax", "Thanos", "punch" ,
+                           "black","hole","cosmic","ray",
+                            "cosmos","crater","comet","red",
+                            "circle","round","bright","dark",
+                            "empty","open","space","outer",
+                            "deep","day","night","dawn",
+                            "dusk","cloud","blue","sound",
+                            "void","white","green","orange"};
 
-    string[] mediumWords = { "spaceship", "Supernova", "Inertia", "Mercury",
-                             "Jupiter", "Sputnik", "Nebula", "Uranus",
-                             "Predator", "Kepler", "Pokemon", "Kuiper",
-                             "Challenger", "Discovery", "Millenium", "Armstrong",
-                             "Quasar", "Light-year", "Guardians", "Martian",
-                             "Kal-El", "Destroyer", "gauntlet", "Falcun"};
+    string[] mediumWords = { "spaceship", "supernova", "inertia", "Mercury",
+                             "Jupiter", "Sputnik", "bebula", "Uranus",
+                             "predator", "kepler", "Pokemon", "kuiper",
+                             "challenger", "discovery", "millenium", "Armstrong",
+                             "quasar", "light-year", "guardians", "Martian",
+                             "Kal-El", "destroyer", "gauntlet", "falcon",
+                             "density","eclipse","velocity","collide",
+                             "horizon","gravity","helium","hydrogen",
+                             "orbit","sidereal","twinkle","little",
+                             "science","infinity","beyond","vacuum",
+                             "bolide","bolometer","cehpeid","clusters",
+                             "element","ozone","perigee","phases"};
 
-    string[] difficultWords = { "Entropy", "Constellation", "Astronomy", "Astrology",
-                                "Magnetron", "Xenomorph", "Kryptoniter", "",
-                                "Astronaut", "Galileo", "Enterprise", "Pathfinder",
-                                "Andromeda", "Astrophysics", "Celestial", "Heliocentric",
-                                "Interstellar", "Syzygy", "Transneptunian", "Rocket Raccoon",
-                                "Frustum"};
-
+    string[] difficultWords = { "entropy", "constellation", "Astronomy", "Astrology",
+                                "magnetron", "Xenomorph", "Kryptoniter", "exoplanet",
+                                "astronaut", "Galileo", "enterprise", "pathfinder",
+                                "Andromeda", "Astrophysics", "celestial", "heliocentric",
+                                "Interstellar", "syzygy", "transneptunian", "Rocket-Raccoon",
+                                "frustum","equinox","earthbound","extragalactic",
+                                "hypernova","telescope","magnitude","meteorite",
+                                "observatory","opposition","occultation","satellite",
+                                "telemetry","terrestrial","wavelength","wormhole",
+                                "interplanetary","pressureless","spacecraft","Milkyway",
+                                "Solar-System","gamma-ray","Acceleration","Atmosphere",
+                                "barycentre","conjunction","coronagraph","ecosphere",
+                                "parallax","perturbations","turbulence","photosphere",
+                                "scintillation","selenography","solstice","stratosphere",
+                                "sundial","Terminator","thermocouple","troposphere",
+                                "antimatter","futuristic","interdimensional","circumlunar"};
+    //"","","",""
     string displayedText;
     Text textBox;
+    public int count;
 
     private void Start()
     {
         currentLevel = LevelData.GetLevel();
+        //currentLevel = 1;
 
         textBox = GetComponent<Text>();
         if (currentLevel == 1)
         {
-            displayedText = EasyRandomWords();
+            displayedText = EasyRandomWords();      
         }
         else if (currentLevel == 2)
         {
@@ -48,6 +73,7 @@ public class WordGenerator : MonoBehaviour
             displayedText = HardRandomWords();
         }
         textBox.text = displayedText;
+        Debug.Log("Level: "+currentLevel);
     }
 
     private void Update()
@@ -59,9 +85,7 @@ public class WordGenerator : MonoBehaviour
     public string EasyRandomWords()
     {
         int ranIndexEasy = Random.Range(0, easyWords.Length);
-
         string easyRandom = easyWords[ranIndexEasy];
-
         return easyRandom;
     }
 
